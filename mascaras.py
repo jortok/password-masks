56e15df369cadec453b4aa8aeac4a6c9af9938df
jortok <tokunag@gmail.com>
2020-06-29 00:15:47 -0500

    Update mascaras.py
    - A bug with non readables chars in the reading file was fixed + "UTF-8"
    - 2 files output are generated, .hcmask to run directly in hashcat & a CSV file.

--- mascaras.py
+++ mascaras.py
@@ -1,5 +1,5 @@
 import collections
-with open("contras.txt","r") as f:
+with open("contras.txt",encoding="utf8",errors="ignore") as f:
     i=0;
     datos=[]
     conteo=[]
@@ -8,21 +8,45 @@ with open("contras.txt","r") as f:
         #datos[largo,máscara,contraseña]
         mascara=""
         for t in linea:
-            if t.isupper(): mascara+="U"
-            elif t.islower(): mascara+="L"
-            elif t.isdigit(): mascara+="D"
-            else: mascara+="S"
+            if t.isupper():
+                mascara+="U"
+            elif t.islower():
+                mascara+="L"
+            elif t.isdigit():
+                mascara+="D"
+            else:
+                mascara+="S"
         datos.append([i,len(linea),linea,mascara])
         conteo.append(mascara)
         i+=1
-#print(collections.Counter(conteo).most_common())
+    archivo=f.name
+
 max_pass=i
 print("Passwords loaded:",max_pass)
 mascaras=collections.Counter(conteo).most_common()
 print("TOP 20 MASK\n===========\n")
 sum_porcentaje=0
-for i in range(0,20):
-    porcentaje=round(100*(mascaras[i][1]/max_pass),2)
-    sum_porcentaje+=porcentaje
-    print(i+1,")",mascaras[i][0],"- Lenght:",len(mascaras[i][0]),"-",mascaras[i][1],"times -",porcentaje,"%")
+
+with open(archivo+".hcmask","w") as f:
+    with open(archivo+".csv","w") as g:
+        cadena="NUM,MASK,HASHCAT MASK,LENGTH,OCURENCES,PERCENT\n"
+        g.write(cadena)
+        for i in range(0,20):
+            porcentaje=round(100*(mascaras[i][1]/max_pass),2)
+            sum_porcentaje+=porcentaje
+            hashcat=""
+            cadena=""
+            for t in mascaras[i][0]:
+                if t=="U":
+                    hashcat+="?u"
+                elif t=="L":
+                    hashcat+="?l"
+                elif t=="D":
+                    hashcat+="?d"
+                else:
+                    hashcat+="?s"
+            print(i+1,")",mascaras[i][0],"-",hashcat,"- Lenght:",len(mascaras[i][0]),"-",mascaras[i][1],"times -",porcentaje,"%")
+            cadena=str(i+1)+","+mascaras[i][0]+","+hashcat+","+str(len(mascaras[i][0]))+","+str(mascaras[i][1])+","+str(porcentaje)+"\n"
+            f.write(hashcat+"\n")
+            g.write(cadena)
 print("\n============\nSUM:",round(sum_porcentaje,2),"%")
